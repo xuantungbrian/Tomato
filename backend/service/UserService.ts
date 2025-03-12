9797de6d22184c3e7cde8a7ba8f7c2759f21d06a
Bryan Tanady <tanadybryan@gmail.com>
2025-03-12 11:39:46 -0700

    test2

--- backend/service/UserService.ts
+++ backend/service/UserService.ts
@@ -12,11 +12,7 @@ export class UserService {
 
     async createUser(id: string, name: string, firebaseToken: string): Promise<IUser|null> {
         try {
-            const newUser: IUser = new UserModel({ _id: id, username: name, firebaseToken })
-            if(!newUser){
-                console.warn("USER NOT CREATED: ", id)
-                return null
-            }
+            const newUser: IUser = new UserModel({ _id: id, username: name, firebaseToken }) as IUser
             return await newUser.save()
         } catch(error) {
             console.error("Error creating user:", error);
@@ -26,7 +22,7 @@ export class UserService {
 
     async getUser(id: string): Promise<IUser|null> {
         try {
-            const user: IUser|null = await UserModel.findById(id)
+            const user: IUser|null = await UserModel.findById(id) as IUser|null
             if(!user){
                 console.warn("USER NOT FOUND: ", id)
                 return null;
@@ -54,7 +50,7 @@ export class UserService {
         }
 
         // Check if user exists, otherwise create a new one
-        const user: IUser|null = await this.getUser(payload.sub);
+        let user: IUser|null = await this.getUser(payload.sub) as IUser|null;
     
         if (!user) {
             user = await this.createUser(payload.sub, payload.name, firebaseToken);
