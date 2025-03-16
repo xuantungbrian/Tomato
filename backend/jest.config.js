2a3e3f59a5ceb2eba4e0d87eebcf8454156fe1b1
rtchi <128873628+rtchi@users.noreply.github.com>
2025-03-15 23:56:02 -0700

    test

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,16 +4,12 @@ module.exports = {
     transform: {
       '^.+\\.tsx?$': 'ts-jest',
     },
-    transformIgnorePatterns: [
-      '/node_modules/', 
-      '<rootDir>/node_modules/some-module/',
-    ],
     moduleFileExtensions: ['ts', 'js', 'json', 'node'],
     testTimeout: 50000,
     collectCoverageFrom: [
       'controllers/*.ts',
       'errors/*.ts',
-      'middleware/*ts',
+      'middleware/*.ts',
       'model/*.ts',
       'service/*.ts',
       'routes/*.ts'
